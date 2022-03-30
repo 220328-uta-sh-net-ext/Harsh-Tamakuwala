f56cf2515b195739d18770353e6b12ace961ac99
Harsh4598 <harsh.ht008@gmail.com>
2022-03-30 16:55:48 -0400

    remove unnecessary variable which hold memory unnecessary

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -5,21 +5,20 @@ echo "--------------------------";
 echo 
 echo "enter number less than 0 to exit from the game..."
 echo
-a=3;
-b=5;
+
 checkNum=0;
 while (( $checkNum >= 0 ));
 do
     read -p "Please enter any num: " checkNum;
 
-    if (($checkNum % $a == 0)) && (($checkNum % $b == 0))
+    if (($checkNum % 3 == 0)) && (($checkNum % 5 == 0))
     then 
         echo "\nfizzbuzz\n";
-    elif (($checkNum % $a == 0))
+    elif (($checkNum % 3 == 0))
     then 
         
         echo "\nfizz\n";
-    elif (($checkNum % $b == 0))
+    elif (($checkNum % 5 == 0))
     then
         echo "\nbuzz\n";
     else
