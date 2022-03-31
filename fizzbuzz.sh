61d9c4f19e5064cf9b5ea3040ac6d4ac524ac7e8
Harsh4598 <harsh.ht008@gmail.com>
2022-03-31 10:16:16 -0400

    add a range of 0 to 20

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -9,22 +9,26 @@ echo
 checkNum=0;
 while (( $checkNum >= 0 ));
 do
-    read -p "Please enter any num: " checkNum;
-
-    if (($checkNum % 3 == 0)) && (($checkNum % 5 == 0))
-    then 
-        echo "fizzbuzz";
-         echo
-    elif (($checkNum % 3 == 0))
-    then    
-        echo "fizz";
-        echo
-    elif (($checkNum % 5 == 0))
-    then  
-        echo "buzz";
-        echo
-    else
-        echo "Nothing";
-        echo
-    fi    
+    read -p "Please enter any number between 0 to 20 : " checkNum;
+    if (( $checkNum < 20 ))
+    then
+        if (($checkNum % 3 == 0)) && (($checkNum % 5 == 0))
+        then 
+            echo "fizzbuzz";
+            echo
+        elif (($checkNum % 3 == 0))
+        then    
+            echo "fizz";
+            echo
+        elif (($checkNum % 5 == 0))
+        then  
+            echo "buzz";
+            echo
+        else
+            echo "Nothing";
+            echo
+        fi 
+    else 
+        echo "Not in range"
+    fi          
 done
