5f6ffb466a6ed21e0db83f1576597038ed705a7f
Harsh4598 <harsh.ht008@gmail.com>
2022-05-10 16:00:19 -0400

    search user by name and get the data

--- project-1/Retaurant_App/RestaurantApi/Controllers/UserController.cs
+++ project-1/Retaurant_App/RestaurantApi/Controllers/UserController.cs
@@ -18,6 +18,8 @@ namespace RestaurantApi.Controllers
        
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<List<UserModelClass>> Get()
         {
            
@@ -39,7 +41,65 @@ namespace RestaurantApi.Controllers
             }
         }
 
-        
+        // GET api/values/5
+        [HttpGet("Search")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<UserModelClass> Get(string searchuser)
+        {
+            if (searchuser == null || int.TryParse(searchuser, out int result) == true)
+                return BadRequest("Invalid input, please try again with valid input");
+            var user = SearchUser.SearchUserAsAdmin(searchuser);
+            if (user.Count <= 0)
+                return NotFound("User not found");
+            return Ok(user);
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult Post([FromBody] UserModelClass user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid User, please try again with valid values.");
+            }
+
+            if (user.FirstName == null || user.FirstName == "")
+            {
+                return BadRequest("Invalid First Name,please enter valid First Name.");
+            }
+            if (user.Password.Length < 6)
+            {
+                return BadRequest("Password must be more than 6 character.");
+            }
+            if (user.ContactNo.ToString().Length != 10)
+            {
+                return BadRequest("Invalid Contact Number,please enter valid contact number.");
+            }
+
+
+            if (user.EmailId == "")
+            {
+                return BadRequest("Invalid Email address,Please enter valid email address!!");
+
+            }
+            try
+            {
+                AddUserLogic.addUserMethod(user);
+                return CreatedAtAction("Get", user);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+
     }
 }
 
