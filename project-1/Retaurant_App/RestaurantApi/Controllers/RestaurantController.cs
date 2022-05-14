690a3632c25a1a80a65702be5ff4f6a149c4b727
Harsh4598 <harsh.ht008@gmail.com>
2022-05-13 21:56:11 -0400

    add restaurant controller and n create getall restaurant api

---
+++ project-1/Retaurant_App/RestaurantApi/Controllers/RestaurantController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantBl;
+using RestaurantModel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestaurantApi.Controllers
+{
+    [Route("api/")]
+    public class RestaurantController : Controller
+    {
+        private RestaurantLogic logic;
+        private SearchRestaurantBl seach;
+        public RestaurantController(RestaurantLogic logic, SearchRestaurantBl seach)
+        {
+            this.seach = seach;
+            this.logic = logic;
+        }
+
+        [Route("getAllRestaurant")]
+        // GET: api/values
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<List<ReviewModelClass>> Get()
+        {
+
+            try
+            {
+                var restaurants = logic.GetAllRestaurant();
+                if (restaurants == null)
+                {
+                    return NotFound("There is no restaurant in Database");
+                }
+                else
+                {
+                    return Ok(restaurants);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Server is down,please try again");
+            }
+        }
+
+        
+    }
+}
+
+
